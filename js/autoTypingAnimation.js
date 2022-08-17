6d72a4637e43c3091f0b40d05e2fa5918268e9a9
Saurabh Kumar Singh <73352093+Saurabh7Goku@users.noreply.github.com>
2022-08-17 23:30:12 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I'm Saurabh Singh </span> an efficient college grad aiming to leverage my abilities to successfully fill the intern role. I am passionate about imbibing new Machine learning models and doing projects. Some of my interests include playing football and I love giving presentations and having good Communication skills, I believe is one of the most important things in my profile. With proven people and leadership skills, I am also very resilient and diligent as a team player. "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
