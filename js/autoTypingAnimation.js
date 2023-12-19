2e7301c3e1920b08cee7e2f3600b8b24762c4208
Saurabh Kumar Singh <73352093+Saurabh7Goku@users.noreply.github.com>
2023-12-19 23:45:17 +0530

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,12 +1,13 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>I'm a passionate Data Scientist and Machine Learning enthusiast with a strong background in developing data-driven solutions and managing end-to-end projects. I specialize in MLOps, version control, and monitoring using tools such as MLFlow, Git, Docker, and more.</span>",
-        "<span>Professional Journey:</span>",
-        "<span>Throughout my career, I've had the privilege of working on exciting projects that have honed my skills and deepened my expertise.</span>",
-        "<span>I'm on a quest to continue contributing my expertise to exciting and impactful projects in the realm of Data Science and Machine Learning. If you're looking for someone who can seamlessly bridge the gap between data and actionable insights while excelling in MLOps and end-to-end project management, let's connect and explore potential collaborations. Your next project might be the one I'm looking for! 😊.</span>"
+        "<span>Hi, I'm Saurabh Singh, an efficient college grad aiming to leverage my abilities to successfully fill the intern role. I am passionate about imbibing new Machine learning models and doing projects. Some of my interests include playing football, and I love giving presentations and having good Communication skills, which I believe is one of the most important things in my profile. With proven people and leadership skills, I am also very resilient and diligent as a team player. <br><br>",
+        "I'm a passionate Data Scientist and Machine Learning enthusiast with a strong background in developing data-driven solutions and managing end-to-end projects. I specialize in MLOps, version control, and monitoring using tools such as MLFlow, Git, Docker, and more.<br><br>",
+        "<strong>Professional Journey:</strong><br>",
+        "Throughout my career, I've had the privilege of working on exciting projects that have honed my skills and deepened my expertise.<br><br>",
+        "I'm on a quest to continue contributing my expertise to exciting and impactful projects in the realm of Data Science and Machine Learning. If you're looking for someone who can seamlessly bridge the gap between data and actionable insights while excelling in MLOps and end-to-end project management, let's connect and explore potential collaborations. Your next project might be the one I'm looking for! 😊.</span>"
       ],
-      typeSpeed: 7,
+      typeSpeed: 10,
       contentType: 'html'
     });
 }, 100);
