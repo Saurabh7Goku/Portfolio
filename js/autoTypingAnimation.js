c20b470a5efa46735cfdbca5ebab7075a70a1bb7
Saurabh Kumar Singh <73352093+Saurabh7Goku@users.noreply.github.com>
2023-12-19 23:50:51 +0530

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -3,7 +3,7 @@ setTimeout(function() {
         strings: [
             "<span>I'm a passionate Data Scientist and Machine Learning enthusiast with a strong background in developing data-driven solutions and managing end-to-end projects. I specialize in MLOps, version control, and monitoring using tools such as MLFlow, Git, Docker, and more. <br>Professional Journey: <br>Throughout my career, I've had the privilege of working on exciting projects that have honed my skills and deepened my expertise. <br>I'm on a quest to continue contributing my expertise to exciting and impactful projects in the realm of Data Science and Machine Learning. If you're looking for someone who can seamlessly bridge the gap between data and actionable insights while excelling in MLOps and end-to-end project management, let's connect and explore potential collaborations. Your next project might be the one I'm looking for! 😊.</span>"
         ],
-        typeSpeed: 7,
+        typeSpeed: 10,
         contentType: 'html'
     });
 }, 100);
